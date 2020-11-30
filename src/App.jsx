88a78ddd19591d8fc43954f1c6cf830fb87e6c8f
sanaeram5 <sanaeram5@gmail.com>
2020-12-01 01:36:39 +0530

    v 1.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Data from './Data';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+
+const App=()=>{
+	return(
+	<div>
+	<Data/>
+	</div>
+	);
+};
+
+export default App;
